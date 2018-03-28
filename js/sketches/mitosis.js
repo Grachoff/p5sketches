fe7f656b645c35d387f812a270a50f5e9f4b873b
grachoff <sergey.grachov@altarix.ru>
2018-03-28 12:31:15 +0300

    sketch complete

--- js/sketches/mitosis.js
+++ js/sketches/mitosis.js
@@ -3,28 +3,28 @@ var mitosisSketch = function( p ) {
     function Cell(position, radius, color) {
         this.pos = position ? position.copy() : p.createVector(p.random(p.width), p.random(p.height));
         this.r = radius ? radius : MAX_RADIUS;
-        this.c = color ? color : p.color(p.random(100, 255), p.random(100, 255), p.random(100, 255), 100);
+        this.c = color ? color : p.color(p.random(0, 255), p.random(0, 255), p.random(0, 255), 100);
 
         this.move = function () {
             var vel = p5.Vector.random2D();
             this.pos.add(vel);
             if (this.r < MAX_RADIUS) this.r+=p.random(0, 0.3);
-        }
+        };
 
         this.show = function () {
             p.noStroke();
             p.fill(this.c, 100);
             p.ellipse(this.pos.x, this.pos.y, this.r, this.r);
-        }
+        };
 
         this.clicked = function (x, y) {
             var d = p.dist(this.pos.x, this.pos.y, x, y);
             return d<this.r;
-        }
+        };
 
         this.mitosis = function () {
             return new Cell(this.pos, this.r/2, this.c);
-        }
+        };
     }
 
 
@@ -42,7 +42,7 @@ var mitosisSketch = function( p ) {
             cells[i].move();
             cells[i].show()
         }
-    }
+    };
     p.mouseClicked = function () {
         for (var i = 0; i < cells.length; i ++) {
             if (cells[i].clicked(p.mouseX, p.mouseY)) {
@@ -53,5 +53,5 @@ var mitosisSketch = function( p ) {
                 return;
             }
         }
-    }
-}
+    };
+};
